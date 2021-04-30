d2b501c442e771a0a821d5aad37357b64fc3020f
Chuela17 <83278932+Chuela17@users.noreply.github.com>
2021-04-30 18:41:10 +0200

    Update barajeo.cpp

--- barajeo.cpp
+++ barajeo.cpp
@@ -2,17 +2,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <vector>
-
+#include <carta.h>
 using namespace std;
 
-int main(){
-   vector<int> baraja(40);
+vector<Carta> barajeo(vector<Carta> baraja){
    int i=0;
-   for(i=0;i<baraja.size();i++){
-       baraja[i]=i+1;
-   }
-   int n;
    time_t t;
+
+   //n = 40;
 
    //Inicialización de srand//
    srand((unsigned) time(&t));
@@ -24,10 +21,10 @@ int main(){
    int n1 = rand() % 40;
    int n2 = rand() % 40;
 
-   int aux=0;
+   Carta aux(0,"");
    aux=baraja[n1];
    baraja[n1]=baraja[n2];
    baraja[n2]=aux;
    }
-   return 0;
+   return baraja;
 }
