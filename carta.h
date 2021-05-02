06606548e1fcb08ac573c03b82da2abdc04115b7
Chuela17 <83278932+Chuela17@users.noreply.github.com>
2021-05-02 20:12:06 +0200

    Update carta.h

--- carta.h
+++ carta.h
@@ -11,11 +11,11 @@ private:
     string direccioncarta;
 
 public:
-    Carta(int, string, string);
-    ~Carta();
-    int GetNumcarta();
-    string GetPalocarta();
-    string GetDireccioncarta();
+    Carta(int, string, string); // Constructor
+    ~Carta(); // Destructor
+    int GetNumcarta(); // Obtener el número de la carta
+    string GetPalocarta(); // Obtener el palo de la carta
+    string GetDireccioncarta(); // Obtener el path de la imagen de la carta
 };
 
 #endif // CARTA_H
