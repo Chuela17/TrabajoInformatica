d0b0b85932ee6ecdb52e80f80cdb2edc367332ed
Chuela17 <83278932+Chuela17@users.noreply.github.com>
2021-05-02 20:17:22 +0200

    Update montoncentro.cpp

--- montoncentro.cpp
+++ montoncentro.cpp
@@ -4,16 +4,35 @@ MontonCentro::MontonCentro():MontonCrece() {
 
 }
 
-MontonCentro::MontonCentro(stack<Carta> cartas, string palo):MontonCrece(cartas) {
+MontonCentro::MontonCentro(stack<Carta> cartas, int id, string palo):MontonCrece(cartas, id) {
     this->palo = palo;
 }
 
+MontonCentro::~MontonCentro() {
+
+}
+
+bool MontonCentro::ComprobarDejarCartaVacio(Carta carta) {
+    return EstaVacio() && carta.GetNumcarta() == 1;
+}
+
+bool MontonCentro::ComprobarDejarCartaSuperior(Carta carta) {
+    return (!EstaVacio() && carta.GetNumcarta() > ConsultarCarta().GetNumcarta())
+            && (carta.GetNumcarta() - ConsultarCarta().GetNumcarta() == 1);
+}
+
+bool MontonCentro::ComprobarDejarCarta(Carta carta) {
+    return this->palo == carta.GetPalocarta()
+            && (this->ComprobarDejarCartaVacio(carta)
+            || this->ComprobarDejarCartaSuperior(carta));
+}
+
 void MontonCentro::DejarCarta(Carta carta) {
-    if (palo != carta.GetPalocarta()) {
+    if (this->palo != carta.GetPalocarta()) {
         throw "Los palos no coinciden, no se puede dejar la carta";
-    } else if (EstaVacio() && carta.GetNumcarta() == 1) {
+    } else if (this->ComprobarDejarCartaVacio(carta)) {
         MontonCrece::DejarCarta(carta);
-    } else if ((carta.GetNumcarta() > ConsultarCarta().GetNumcarta()) && (carta.GetNumcarta() - ConsultarCarta().GetNumcarta() == 1)) {
+    } else if (this->ComprobarDejarCartaSuperior(carta)) {
         MontonCrece::DejarCarta(carta);
     } else {
         throw "No se puede dejar la carta";
